7945a51a0325f6401fdeec5260aaf91f251d5f5d
Lavesari <Lavesari@MBP-de-guido.home>
2026-02-15 22:32:07 +0100

    Arreglando ruta router

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx'
 import CVBuilder from './CVBuilder.jsx'; // Importamos tu nuevo componente
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* AÑADE ESTA PARTE QUE DICE basename="/jobhunter-crm" */}
+    <BrowserRouter basename="/jobhunter-crm">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/cv" element={<CVBuilder />} />
